6900d959f064082fe71a6406d788ac628402fe3d
Josh Anderson <joshua.frederick.anderson@gmail.com>
2018-12-27 03:35:57 -0800

    wip checkpoint at 'the roomlist component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyC71AskW9FcL46lkUhZC_9jKUL2HDPjvyA",
+  authDomain: "bloc-chat-4708d.firebaseapp.com",
+  databaseURL: "https://bloc-chat-4708d.firebaseio.com",
+  projectId: "bloc-chat-4708d",
+  storageBucket: "bloc-chat-4708d.appspot.com",
+  messagingSenderId: "42802058630"
+};
+firebase.initializeApp(config);
+
 
 class App extends Component {
   render() {
