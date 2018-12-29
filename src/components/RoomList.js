0602dcac5593c9763c015bbaa1fc74137eda4850
Josh Anderson <joshua.frederick.anderson@gmail.com>
2018-12-28 20:49:09 -0800

    Creating  RoomList.js

---
+++ src/components/RoomList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class RoomList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      room: []
+    }
+    
+  }
+}
+
+export default RoomList;
