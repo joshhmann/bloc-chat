8bd03550cf0c87f8b3d8ffee67413916814ffac6
Josh Anderson <joshua.frederick.anderson@gmail.com>
2018-12-31 18:30:05 -0800

    Rendered rooms. Needed to add firebase as prop in app.js

--- src/App.js
+++ src/App.js
@@ -19,8 +19,8 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-      <RoomList />
+      <div>
+      <RoomList firebase={firebase}/>
       </div>
     );
   }
