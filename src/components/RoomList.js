30b82141111fded70ef72e95e67541e3e5b14c84
Josh Anderson <joshua.frederick.anderson@gmail.com>
2019-01-02 09:01:08 -0800

    Rooms created

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -4,10 +4,22 @@ class RoomList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      rooms: []
+       name: "", rooms: []
     };
 
     this.roomsRef = this.props.firebase.database().ref('rooms');
+    this.handleChange = this.handleChange.bind(this);
+    this.createRoom = this.createRoom.bind(this)
+  }
+
+  handleChange(e) {
+    this.setState({ name: e.target.value});
+  }
+
+  createRoom(e) {
+    e.preventDefault();
+    this.roomsRef.push({ name: this.state.name});
+    this.setState({ room: ""});
   }
 
   componentDidMount() {
@@ -19,15 +31,24 @@ class RoomList extends Component {
   }
 
   render() {
+    const roomForm = (
+      <form onSubmit={this.createRoom}>
+        <input type="text" value={this.state.name} placeholder="Enter Room Name" onChange={this.handleChange}/>
+        <input type="submit" value="Create" />
+      </form>
+    );
+
     const roomList = this.state.rooms.map((room) =>
       <li key={room.key}>{room.name}</li>
     );
 
     return(
-      <ul>{roomList}</ul>
+      <div>
+        <div>{roomForm}</div>
+        <ul>{roomList}</ul>
+      </div>
     );
   }
-
 }
 
 export default RoomList;
